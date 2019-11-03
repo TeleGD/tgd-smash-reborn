6fb38f94cc1cd232523f1d56f91c1e69314ec704
stephane-hulot <hulot.stephane.55@gmail.com>
2019-11-03 20:07:58 +0100

    Changement des shaders et correction des attaques

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,22 +19,18 @@ public class PlayerController : MonoBehaviour
 	private float runRatio;
 
 	// est en train de se faire tapper (ou d'en sentir les conséquences)
-	private bool punched;
+	private bool isPunched;
+    private bool isPunching;
 
     private Animator animator;
     public GameObject model; //c'est le GameObject "Model" du perso
-    private int punchingFrame=0;
 	private int beeingPunchFrame = 0;
-
-	// Joueur a porté d'attaque
-	private GameObject closePlayer;
-	// Le dernier attaquant dont on subit encore l'attaque
-	private GameObject attacker;
-	private int lives;
-    public GameObject textMeshPro;
-	public GameObject heartPrefab;
+    
+	private int lives; //nombre de coeurs
 
     private float lastExitTime; //temps a la derniere sortie de la zone
+
+    public Rect zone; //TODO : lire ces infos sur un objet unique dans la scene
 
     private void Start()
 	{
@@ -45,6 +41,7 @@ public class PlayerController : MonoBehaviour
 		runRatio = 2.5f;
         animator = model.GetComponent<Animator>();
         lives = 5;
+        ToggleAttackTriggers(false);
 	}
 
     private void Update()
@@ -60,39 +57,24 @@ public class PlayerController : MonoBehaviour
 		// Si le bouton d'attaque est pressé
 		if (Input.GetButtonDown("Attack"+ playerID))
 		{
-			if (!animator.GetBool("punching"))
-            {
-                animator.SetBool("punching", true);
-                Attack();
-                
-            }
-        }
-
-		// Gestion de l'animation de notre attaque
-        if (animator.GetBool("punching"))
-        {
-
-            if (punchingFrame == 15)
-            {
-                animator.SetBool("punching", false);
-                punchingFrame = -1;
-
-			}
-				
-            punchingFrame++;
+			if (!isPunching)
+                StartCoroutine(Attack());
         }
 
 		// Si on nous attaque
-		if (punched)
+		if (isPunched)
 		{
 			if(beeingPunchFrame > 15)
 			{
-				attacker = null;
-				punched = false;
+				isPunched = false;
 				beeingPunchFrame = -1;
 			}
 			beeingPunchFrame++;
 		}
+
+        Vector2 pos = transform.position;
+        if (pos.x < zone.xMin || pos.x > zone.xMax || pos.y > zone.yMax || pos.y < zone.yMin)
+            KillPlayer();
 	}
 
 	private void FixedUpdate()
@@ -117,7 +99,7 @@ public class PlayerController : MonoBehaviour
 			rb.AddForce(new Vector2(dir, 0));
 		}
 		//freine le joueur si on relache la direction
-		if (Mathf.Abs(Input.GetAxis("Horizontal" + playerID)) < 0.4f || punched)
+		if (Mathf.Abs(Input.GetAxis("Horizontal" + playerID)) < 0.4f || isPunched)
 		{
 			rb.AddForce(new Vector2(-rb.velocity.x * 10, 0));
 		}
@@ -131,84 +113,92 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D coll)
+    private void OnTriggerEnter2D(Collider2D coll)
 	{
-		// Si le collider est un collider d'attaque (le bras de l'adversaire)
-		if (coll.CompareTag("Attack"))
-		{
-			// On récupère le joueur proche
-			closePlayer = coll.transform.root.gameObject;
-		}
+        // Si le collider est un collider d'attaque (le bras de l'adversaire)
+        if (coll.CompareTag("Attack"))
+            PlayerHit(Mathf.Sign(transform.position.x - coll.transform.parent.position.x));
+    }
+
+	// Notre joueur vient de se faire taper
+	private void PlayerHit(float xDir)
+	{
+        if (this.isPunched)
+            return;
+
+		SetPunched(true);
+
+		// force du coup ne dépendant pas de la vitesse
+		float punchForce = 30f;
+
+		// 10 est un coefficient permettant d'accentuer l'importance de la vélocité dans le calcul de la force du coup
+		float playerVel = Mathf.Abs(rb.velocity.x) * 10;
+
+		rb.AddForce(new Vector2(xDir * (playerVel + punchForce), (playerVel + punchForce) * 0.1f),ForceMode2D.Impulse);
 	}
 
-	// Attack another player
-	private void Attack()
-	{
-		if (closePlayer != null)
-		{
-			PlayerController otherPlayerController = closePlayer.GetComponent<PlayerController>();
-			otherPlayerController.setPunched(true);
-
-			// force du coup ne dépendant pas de la vitesse
-			float punchForce = 15f;
-
-			// 13 est un coefficient permettant d'accentuer l'importance de la vélocité dans le calcul de la force du coup
-			float playerVel = Mathf.Abs(rb.velocity.x) * 5;
-
-
-			// On calcule où se situe le joueur par rapport à l'autre
-			float playersPositionDiff = transform.position.x - otherPlayerController.transform.position.x;
-
-
-			float xDirection;
-			// l'autre joueur est à droite du joueur
-			if (playersPositionDiff > 0)
-				xDirection = -1;
-			else if (playersPositionDiff < 0)
-				xDirection = 1;
-			else
-				xDirection = 0;
-
-			//Vector3 movement = new Vector3(xDirection, 0.0f, 0);
-			otherPlayerController.rb.AddForce(new Vector2(xDirection * (playerVel + punchForce), (playerVel + punchForce) * 0.1f),ForceMode2D.Impulse);
-
-		}
-	}
-
-    private void OnTriggerExit2D(Collider2D coll)
+    private void KillPlayer()
     {
-		if (coll.CompareTag("Attack"))
-		{
-			closePlayer = null;
-		}
-		// Sortie de la zone : elle est représentée par un rectangle comprenant toute la zone.
-		// La sortie du trigger correspond à une sortie de la zone.
-		if (coll.CompareTag("Zone") && lastExitTime + 1 < Time.time)
+		if(lastExitTime + 1 < Time.time)
         {
             lastExitTime = Time.time;
             Debug.Log(gameObject.name + " est sorti de la zone");
             transform.position = new Vector3(0, 2, 0);
 			rb.velocity = new Vector3(0, 0, 0);
-			lives = lives>0 ? lives-1 : lives;
+			lives--;
             GameManager.instance.UpdateHearts(playerID, lives);
 
 
-            if (lives==0)
+            if (lives<=0)
             {
                 GameMaster.KillPlayer(this);
-                
                 Destroy(gameObject);
             }
         }
 
     }
 
-	public int GetID() {
-        return playerID;
+    //Coroutine a lancer pour l'attaque du joueur
+    private IEnumerator Attack()
+    {
+        isPunching = true;
+        animator.SetBool("punching", true);
+        yield return new WaitForSeconds(0.1f);
+        ToggleAttackTriggers(!isPunched); //active les triggers...
+        yield return new WaitForSeconds(0.2f);
+        ToggleAttackTriggers(false); //...puis les desactive
+        yield return new WaitForSeconds(0.2f);
+        animator.SetBool("punching", false);
+        isPunching = false;
     }
 
-	public void setPunched(bool punched)
+    //Active ou non les trigger d'attaque de ce joueur
+    private void ToggleAttackTriggers(bool actived)
+    {
+        transform.GetChild(0).gameObject.SetActive(actived);
+        transform.GetChild(1).gameObject.SetActive(actived);
+    }
+
+	public int GetID()
+    {
+        return playerID;
+    }
+    
+	public void SetPunched(bool punched)
 	{
-		this.punched = punched;
+        StopAllCoroutines();
+        animator.SetBool("punching", false);
+        isPunching = false;
+        ToggleAttackTriggers(false);
+        this.isPunched = punched;
 	}
+
+    private void OnDrawGizmos()
+    {
+        //Affiche la zone dans le viewport de la scene
+        Gizmos.DrawLine(new Vector3(zone.xMin, zone.yMin, 0), new Vector3(zone.xMax, zone.yMin, 0));
+        Gizmos.DrawLine(new Vector3(zone.xMin, zone.yMax, 0), new Vector3(zone.xMax, zone.yMax, 0));
+        Gizmos.DrawLine(new Vector3(zone.xMin, zone.yMin, 0), new Vector3(zone.xMin, zone.yMax, 0));
+        Gizmos.DrawLine(new Vector3(zone.xMax, zone.yMin, 0), new Vector3(zone.xMax, zone.yMax, 0));
+    }
 }
